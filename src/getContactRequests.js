83fe4c9c8e57a608035387abbe1320e60e4cd09a
Denis <st.denis@me.com>
2019-04-01 18:41:07 +0800

    removed redundant files

--- src/getContactRequests.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (transport, dapId, userId) => {
-  var a = await transport.fetchDapObjects(dapId, 'contact', { blockchainUserId: userId });
-  var b = await transport.fetchDapObjects(dapId, 'user', { 'object.user': userId });
-  return [a, b];
-};
