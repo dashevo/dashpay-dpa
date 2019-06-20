465e3d1d33bd80aa3b1f387b1f574966bd52fd22
Denis <st.denis@me.com>
2019-06-20 13:17:25 +0300

    fix: profile register getBUserSigningPrivateKey

--- src/Profile/register.js
+++ src/Profile/register.js
@@ -1,22 +1,28 @@
 module.exports = async function register() {
   if (!this.buser) throw new Error('Require to first attach a buser to profile');
-  if (this.buser.state !== 'open') throw new Error('Expected state to be open. Are you sync ?');
-  if (!this.buser.isOwned) throw new Error('Expected BUser to be owned. Did you try `own(privKey`) ?');
-  if (!this.buser.dpp) {
+  const { buser } = this;
+  if (buser.state !== 'open') throw new Error('Expected state to be open. Are you sync ?');
+  if (!buser.isOwned) throw new Error('Expected BUser to be owned. Did you try `own(privKey`) ?');
+  if (!buser.dpp) {
     throw new Error('Missing dpp - Did you attach to a buser ?');
   }
   // We prepare our object
-  const profile = this.buser.dpp.document.create('profile', {
+  const profile = buser.dpp.document.create('profile', {
     avatarUrl: this.avatar,
     about: this.bio,
   });
 
-  console.log('Validating profile contract', this.buser.dpp.document.validate(profile)
+  console.log('Validating profile contract', buser.dpp.document.validate(profile)
     .isValid());
   const {
     serializedTransaction,
     serializedPacket,
-  } = this.prepareStateTransition(profile, this.buser, this.getBUserSigningPrivateKey(), this);
+  } = this.prepareStateTransition(
+    profile,
+    buser,
+    buser.getBUserSigningPrivateKey(),
+    this,
+  );
 
 
   const txid = await this.broadcastTransition(
@@ -24,6 +30,6 @@ module.exports = async function register() {
   );
   // const txid = 0;
 
-  console.log(`Profile ${this.buser.username}  Registered (txid ${txid}.`);
+  console.log(`Profile ${buser.username}  Registered (txid ${txid}.`);
   return txid;
 };
