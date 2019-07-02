852a5f204b5825cbb93bbae4a45da6685578e8dc
Alex Werner <alex@werner.fr>
2019-07-02 09:09:35 +0200

    feat(own): verify if valid privatekey

--- src/BUser/own.js
+++ src/BUser/own.js
@@ -1,7 +1,21 @@
-module.exports = function own(privateKey) {
-  console.log('Owning the BUser with privatekey', privateKey.toString());
+const { PrivateKey, util } = require('@dashevo/dashcore-lib');
+
+module.exports = function own(_privateKey) {
+  console.log('Owning the BUser with privatekey', _privateKey.toString());
+  const privateKey = (typeof _privateKey === 'string')
+    ? new PrivateKey(_privateKey)
+    : _privateKey;
+
+
+  // eslint-disable-next-line no-underscore-dangle
+  const pubkeyid = util.buffer.copy(privateKey.toPublicKey()
+    ._getID()).reverse().toString('hex');
+  if (this.pubkeyid && this.pubkeyid !== pubkeyid) {
+    console.error(`Mismatch pubkeyid. Expected ${this.pubkeyid} got: ${pubkeyid}`);
+    throw new Error('Invalid privateKey for this buser. Cannot own');
+  } else {
+    console.log('Own without being able to verify. Sync before owning to get verification.');
+  }
   this.privateKey = privateKey.toString();
-  // Todo : Either here or on synchronize method, we should actually test if that is a
-  // valid key :)
   this.isOwned = true;
 };
